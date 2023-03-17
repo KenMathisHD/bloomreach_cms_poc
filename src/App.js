8d3676f995951a2971a5e08a435cb0a896f1d00b
Simpson <ksimpson@bjs.com>
2023-03-17 11:29:52 -0400

    fixed issue with sass not working due to older react script being loaded

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+// External
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { BrPage } from "@bloomreach/react-sdk";
 import { ContentPage } from "./components/ContentPage";
+
+// Components
 import { KenTestComponent } from "./components/KenTestComponent";
 import { SimpleImageComponent } from "./components/SimpleImageComponent";
 import { OpenHTML } from "./components/OpenHTML";
+
+// CSS/Assets
+import "./dc_mod.scss";
 
 const kenTestToken = "d393d9a9-caac-4b3b-b173-014c36b250d7";
 
@@ -16,16 +19,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <BrPage
           configuration={{
             path: `${window.location.pathname}${window.location.search}`,
             endpoint:
-              "https://trial-xdfd57qq.bloomreach.io/delivery/site/v1/channels/home-poc-refspa/pages",
+              "https://trial-xdfd57qq.bloomreach.io/delivery/site/v1/channels/ken-test-channel/pages",
             httpClient: axios,
           }}
           mapping={{
@@ -41,4 +39,3 @@ function App() {
 }
 
 export default App;
-
